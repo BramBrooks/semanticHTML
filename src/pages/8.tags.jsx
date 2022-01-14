68265007891cc6099e5f8ef04978451699fbe036
Bram Brooks <bram451@hotmail.com>
2022-01-14 14:34:09 +0000

    updates to responsive view of tags page

--- src/pages/8.tags.jsx
+++ src/pages/8.tags.jsx
@@ -3,22 +3,28 @@ import TagTable from "../components/tagtable";
 
 const Tag = () => {
   return (
-    <main>
-      <section>
-        <h2>Tags.</h2>
-        <p className="p">
+    <div className="tag_page_container">
+      {" "}
+      <main>
+        <section>
+          <h2>
+            Tags&nbsp;<span>included</span>&nbsp;in this project: &nbsp;
+            <span>81%</span>&nbsp;
+          </h2>
+          {/* <p className="p">
           Below is a list of currently HTML elements (aside from those which are
           now defunct).
           <br />
           <br />
           Tags marked in <mark>red</mark> indicate tag has been included in this
           project so far...
-        </p>
-        <div className="tag_table_container">
-          <TagTable />
-        </div>
-      </section>
-    </main>
+        </p> */}
+          <div className="tag_table_container">
+            <TagTable />
+          </div>
+        </section>
+      </main>
+    </div>
   );
 };
 

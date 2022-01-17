afd5f62e63e38f724e5d42fb20137e97a527dcaf
Bram Brooks <bram451@hotmail.com>
2022-01-17 15:54:31 +0000

    updates header file to remove build error

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../media/images/rediffusion_logo.png";
 import Nav from "./navbar/nav";
